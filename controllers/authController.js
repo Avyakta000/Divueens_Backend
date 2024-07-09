d2e1a4cbd0bb5892531819dfb329e244acecea80
Avyakta000 <100035215+Avyakta000@users.noreply.github.com>
2024-07-10 00:49:42 +0530

    commiting to resolve JWT_SECRET undefined

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ const login =  async (req, res) => {
       const isMatch = await bcrypt.compare(password, user.password);
       if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
-      const authToken = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '7 days' });
+      const authToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' });
       res.json({ message: "login successful", authToken });
   } catch (error) {
       res.status(500).json({ error: error.message });
